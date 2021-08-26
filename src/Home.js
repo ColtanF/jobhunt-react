0f78f70e13e2556411078acb318578f579bec7b5
ColtanF <coltan.franke@gmail.com>
2021-08-26 16:36:46 -0500

    Trying to fix location conditional again.

--- src/Home.js
+++ src/Home.js
@@ -8,14 +8,18 @@ export default function Home() {
   return (
     <main>
       <section className="ui container center aligned">
-        {location?.state.action && location.state.action === "registered" && (
-          <Message success>
-            User registered successfully. Please log in.
-          </Message>
-        )}
-        {location?.state.action && location.state.action === "logged_in" && (
-          <Message success>User logged in successfully.</Message>
-        )}
+        {location.state &&
+          location.state.action &&
+          location.state.action === "registered" && (
+            <Message success>
+              User registered successfully. Please log in.
+            </Message>
+          )}
+        {location.state &&
+          location.state.action &&
+          location.state.action === "logged_in" && (
+            <Message success>User logged in successfully.</Message>
+          )}
         <h1>Welcome to JobHunt</h1>
         <h2 className="header">
           Go to the <Link to="/jobs">Jobs page</Link> or
